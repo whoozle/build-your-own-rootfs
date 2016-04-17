4112e436b757a1d4543bb5a9792159777ed9e58e
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2016-04-17 14:06:51 +0100

    added layout scripts to layout/ directory, added platform stub

---
+++ layout/common/etc/init.d/S00boot.sh
@@ -0,0 +1,9 @@
+echo "mounting /proc"
+mount -t proc proc /proc
+echo "mounting /sys"
+mount -t sysfs sysfs /sys
+echo "switching to mdev"
+echo /sbin/mdev > /proc/sys/kernel/hotplug
+echo "populating devices from sysfs"
+mdev -s
+echo "boot done"
