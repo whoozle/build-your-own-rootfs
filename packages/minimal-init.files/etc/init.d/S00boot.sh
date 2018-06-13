5f62261f93cbd838b3e2cd55fa4e3edd057709b9
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2018-06-13 23:27:28 +0100

    added minimal generic init script

---
+++ packages/minimal-init.files/etc/init.d/S00boot.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "mounting /dev"
+mount -t devtmpfs dev /dev
+echo "mounting /proc"
+mount -t proc proc /proc
+echo "mounting /sys"
+mount -t sysfs sysfs /sys
+echo "switching to mdev"
+echo /sbin/mdev > /proc/sys/kernel/hotplug
+echo "populating devices from sysfs"
+mdev -s
+echo "boot done"
